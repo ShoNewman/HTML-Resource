d08416b287024ea7b41039a101619e16f01a74c0
Shoshana Newman <sho.newman@gmail.com>
2021-06-01 15:03:03 -0700

    Add jquery to toggle img
    Co-authored-by: Ben Pawlik <james.benjamin.pawlik@gmail.com>
    Co-authored-by: Ben Pawlik <james.benjamin.pawlik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
+// HTML help page
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a heading.");
-  });
+//   $("h1").click(function() {
+//     alert("This is a heading.");
+//   });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
 
-  $("img").click(function() {
-    alert("This is an image.");
-  });
+//   $("img").click(function() {
+//     alert("This is an image.");
+//   });
+
+
+// walrus page
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle('slow');
+    $("#walrus-hidden").toggle('slow');
+    $("#hideImg").toggle();
+    $("#showImg").toggle();
+    $("img").slideOut('slow');
+  })
+  $('.button').click(function() {
+    $("#walrus-showing").toggle('slow');
+    $("#walrus-hidden").toggle('slow');
+  })
+  
 });
